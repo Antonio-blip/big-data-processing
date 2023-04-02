854e151d4eb893af830ef5679633d8c93046bfeb
Antonio-blip <63549493+Antonio-blip@users.noreply.github.com>
2023-04-02 22:33:58 +0200

    Add files via upload

---
+++ data/modular_read_data_variables.py
@@ -0,0 +1,11 @@
+# Global constants to be able to change the data
+CSV_DATA_BACKUP = 'data/world-happiness-report.csv'
+CSV_DATA_LAST_YEAR = 'data/world-happiness-report-2021.csv'
+# Last year for questions that needs it
+LAST_YEAR = 2021
+# Used in question 4 Happy ranking against GDP
+Q4_YEAR_SELECTOR = 2020
+# Used in question 5 to compare the GPD average between the last year and the chosen year "
+Q5_YEAR_SELECTOR = 2020
+# Used in question 6 to compare the country with the highest life expectancy at birth in the last year to the chosen year"
+Q6_YEAR_SELECTOR = 2019
